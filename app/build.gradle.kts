ae5ad255a8e0be216cbb857c1fe31c1ba6726670
Alpine Lake <alpinelake105@gmail.com>
2025-02-13 12:55:18 -0800

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.example.orion"
         minSdk = 33
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
